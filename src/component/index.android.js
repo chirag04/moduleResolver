a2f12603d2b935320a4ccebe085a28e90f300157
Chirag Jain <jain_chirag04@yahoo.com>
2016-11-14 17:57:33 -0500

    initial commit

---
+++ src/component/index.android.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Foo from 'foo';
+
+export default () => (
+	<View>
+		<Text>component android</Text>
+		<Foo />
+	</View>
+);
---
+++ src/component/index.ios.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Foo from 'foo';
+
+export default () => (
+	<View>
+		<Text>component ios</Text>
+		<Foo />
+	</View>
+);
